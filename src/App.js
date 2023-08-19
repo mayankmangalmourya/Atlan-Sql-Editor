990c7ab6de98ae042d259132c1510fa845d6fd46
Mayank Mangal Mourya <Shantanumourya8912@gmail.com>
2023-08-19 09:44:22 +0530

    queryspace created and codemirror error also resolve

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './header';
 import sideBar from './sideBar';
+import querySpace from './querySpace';
 
 function App() {
   return (
     <>
     <Header />
     <sideBar/>
+    <querySpace />
     </>
   );
 }
--- src/querySpace.js
+++ src/querySpace.js
@@ -1,4 +1,8 @@
 import { Fragment } from "react";
+import { UnControlled as CodeMirror } from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/rubyblue.css";
+import { getPropTypes } from "./data/utils";
 
 function querySpace(params) {
     return(
@@ -16,5 +20,7 @@ function querySpace(params) {
 
     );
 }
+
+querySpace.propTypes = getPropTypes("defaultQuery");
 
 export default querySpace;
